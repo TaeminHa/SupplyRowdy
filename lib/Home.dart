3b6b2ab71896533a8fb84a26a5dec22674cb0ae0
MichaelMohn <50528867+MichaelMohn@users.noreply.github.com>
2020-03-29 08:37:27 -0500

    Home, Supply, Info Pages

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,41 +1,49 @@
 import 'package:flutter/material.dart';
 import 'SupplyPage.dart';
 
+
+
+
 class Home extends StatelessWidget {
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.green,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/CitiesLights.PNG"),
+            fit: BoxFit.cover,
+          ),
+        ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
-                ButtonTheme(
-                  minWidth: 200,
-                  child: RaisedButton(
-                    child: Text(
-                      'NAME'
+                FlatButton(
+                  color: Colors.black,
+                  child: Text(
+                    "Lets get Started",
+                    style: TextStyle(
+                      color: Colors.white
                     ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SupplyPage()),
-                      );
-                    },
                   ),
-                ),
-                SizedBox(
-                  height: 20,
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => (SupplyPage())));
+                  },
                 )
               ],
-            ),
-          ],
-        ),
-      )
 
+            )
+          ],
+        )
+      ),
     );
   }
 }
